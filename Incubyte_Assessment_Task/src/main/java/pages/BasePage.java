3ba040844921640d8f3f043d5ecd0998ec54e60c
Atul Patil <atulp441@gmail.com>
2025-03-24 23:23:35 +0530

    write BasePage

--- Incubyte_Assessment_Task/src/main/java/pages/BasePage.java
+++ Incubyte_Assessment_Task/src/main/java/pages/BasePage.java
@@ -1,5 +1,64 @@
-package main.java.pages;
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
 public class BasePage {
+    protected WebDriver driver;
+    private WebDriverWait wait;
 
+    // Constructor
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        PageFactory.initElements(driver, this);
+    }
+
+    // Common method to open a URL
+    public void openUrl(String url) {
+        driver.get(url);
+    }
+
+    // Wait until the element is visible
+    public void waitForVisibility(WebElement element) {
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    // Wait until the element is clickable
+    public void waitForClickability(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    // Click an element
+    public void click(WebElement element) {
+        waitForClickability(element);
+        element.click();
+    }
+
+    // Enter text into an input field
+    public void enterText(WebElement element, String text) {
+        waitForVisibility(element);
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    // Get the text of an element
+    public String getElementText(WebElement element) {
+        waitForVisibility(element);
+        return element.getText();
+    }
+
+    // Verify if an element is displayed
+    public boolean isElementDisplayed(WebElement element) {
+        try {
+            waitForVisibility(element);
+            return element.isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
