1fb5d5ae31bab027bcc0414d5b70f475479a9283
Atul Patil <atulp441@gmail.com>
2025-03-24 23:28:58 +0530

    added Login page locators and actions

--- Incubyte_Assessment_Task/src/main/java/pages/LoginPage.java
+++ Incubyte_Assessment_Task/src/main/java/pages/LoginPage.java
@@ -1,5 +1,54 @@
 package main.java.pages;
 
-public class LoginPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
+public class LoginPage extends BasePage {
+
+    // Constructor
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    // Web Elements using @FindBy annotation
+    @FindBy(id = "email")
+    private WebElement emailInput;
+
+    @FindBy(id = "pass")
+    private WebElement passwordInput;
+
+    @FindBy(id = "send2")
+    private WebElement signInButton;
+
+    @FindBy(xpath = "//div[contains(@class, 'message-error')]")
+    private WebElement errorMessage;
+
+    @FindBy(xpath = "//div[contains(@class, 'message-success')]")
+    private WebElement successMessage;
+
+    // Page Actions
+    public void openLoginPage() {
+        openUrl("https://magento.softwaretestingboard.com/customer/account/login/");
+    }
+
+    public void enterEmail(String email) {
+        enterText(emailInput, email);
+    }
+
+    public void enterPassword(String password) {
+        enterText(passwordInput, password);
+    }
+
+    public void clickSignIn() {
+        click(signInButton);
+    }
+
+    public boolean isLoginSuccessful() {
+        return isElementDisplayed(successMessage);
+    }
+
+    public boolean isLoginFailed() {
+        return isElementDisplayed(errorMessage);
+    }
 }
